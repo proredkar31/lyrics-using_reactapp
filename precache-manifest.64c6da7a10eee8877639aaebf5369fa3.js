095d554dbb4724e8821df40b8f55d9fcc9cbe65c
Pramod <proredkar31@users.noreply.github.com>
2020-03-25 00:21:23 +0530

    Updates

---
+++ precache-manifest.64c6da7a10eee8877639aaebf5369fa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4bc21d007bb57d2477ee619c174c710",
+    "url": "/lyrics-using_reactapp/index.html"
+  },
+  {
+    "revision": "7e2bce73fc757ce2ce7a",
+    "url": "/lyrics-using_reactapp/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "a295a4f6ded0cc3d636f",
+    "url": "/lyrics-using_reactapp/static/js/2.b310d276.chunk.js"
+  },
+  {
+    "revision": "228bac5b7587832345b091357f4a52ce",
+    "url": "/lyrics-using_reactapp/static/js/2.b310d276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e2bce73fc757ce2ce7a",
+    "url": "/lyrics-using_reactapp/static/js/main.4568afc3.chunk.js"
+  },
+  {
+    "revision": "078f07a427e2710be0b2",
+    "url": "/lyrics-using_reactapp/static/js/runtime-main.56713879.js"
+  }
+]);
